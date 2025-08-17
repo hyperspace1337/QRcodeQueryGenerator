5133c8d2a4e4fd26bd71d7962d116e99055ddaa6
hyperspace1337 <skillproj@mail.ru>
2025-08-17 23:54:09 +0300

    Minor translation update

--- backend/api/v1/consumables.py
+++ backend/api/v1/consumables.py
@@ -16,19 +16,19 @@ from config import assets_dir
 
 router = APIRouter(
     prefix="/consumables",
-    tags=["Расходники"],
+    tags=["Consumables"],
 )
 
-@router.post("/", summary="Создать расходник")
+
+@router.post("/", summary="Create a consumable")
 def create_consumable(consumable: ConsumableAddSchema,
                       db: Session = Depends(get_db),
                       lang: str = Depends(get_lang)):
-
     new_consumable = ConsumableModel(
-        name = consumable.name,
-        erp_code = consumable.erp_code,
-        qty = consumable.qty,
-        department = consumable.department
+        name=consumable.name,
+        erp_code=consumable.erp_code,
+        qty=consumable.qty,
+        department=consumable.department
     )
 
     db.add(new_consumable)
@@ -39,10 +39,10 @@ def create_consumable(consumable: ConsumableAddSchema,
             "ok": True,
             "message": MESSAGES[lang]["consumable.created"]}
 
-@router.get("/", summary="Получить список расходников")
+
+@router.get("/", summary="Get a consumable list")
 def get_consumables(lang: str = Depends(get_lang),
                     db: Session = Depends(get_db)):
-
     query = select(ConsumableModel)
     data = db.execute(query)
 
@@ -50,40 +50,40 @@ def get_consumables(lang: str = Depends(get_lang),
             "ok": True,
             "message": MESSAGES[lang]["consumable.get_all"]}
 
-@router.get("/{consumable_id}", summary="Получить расходник")
+
+@router.get("/{consumable_id}", summary="Get a consumable")
 def get_consumable(lang: str = Depends(get_lang),
                    consumable: ConsumableModel = Depends(check_consumable_exists)):
     return {"data": consumable,
             "ok": True,
             "message": MESSAGES[lang]["consumable.get"]}
 
+
 @router.get("/{consumable_id}/qrcode",
-            tags = ["Расходники", "QR-коды"],
-            summary="Получить QR-код для расходника")
-
+            tags=["Consumables", "QR-codes"],
+            summary="Get consumable QR-code")
 def get_qrcode(consumable: ConsumableModel = Depends(check_consumable_exists)):
+    qr = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_H)
+    qr.add_data(consumable.id)
+    qr_img = qr.make_image(image_factory=StyledPilImage, embedded_image_path=assets_dir / "AE.png")
 
-        qr = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_H)
-        qr.add_data(consumable.id)
-        qr_img = qr.make_image(image_factory=StyledPilImage, embedded_image_path= assets_dir/"AE.png")
+    buffer = BytesIO()
+    qr_img = qr_img.resize((500, 500))
+    qr_img.save(buffer, format="PNG")
+    buffer.seek(0)
 
-        buffer = BytesIO()
-        qr_img = qr_img.resize((500, 500))
-        qr_img.save(buffer, format="PNG")
-        buffer.seek(0)
+    return Response(content=buffer.read(), media_type="image/png")
 
-        return Response(content=buffer.read(), media_type="image/png")
 
-@router.patch("/{consumable_id}", tags=["Расходники"], summary="Обновить расходник")
+@router.patch("/{consumable_id}", tags=["Consumables"], summary="Update a consumable")
 def update_consumable(patch_data: ConsumablePatchSchema,
                       lang: str = Depends(get_lang),
                       db: Session = Depends(get_db),
                       consumable: ConsumableModel = Depends(check_consumable_exists)):
-
     consumable_patch = patch_data.model_dump(exclude_unset=True)
 
     if not consumable_patch:
-        raise HTTPException(status_code=400, detail="Данные для обновления не переданы")
+        raise HTTPException(status_code=400, detail=MESSAGES[lang]["common.no_upd_data"])
 
     for key, value in consumable_patch.items():
         setattr(consumable, key, value)
@@ -96,7 +96,8 @@ def update_consumable(patch_data: ConsumablePatchSchema,
             "ok": True,
             "message": MESSAGES[lang]["consumable.created"]}
 
-@router.delete("/{consumable_id}", tags=["Расходники"], summary="Удалить расходник")
+
+@router.delete("/{consumable_id}", tags=["Consumables"], summary="Delete a consumable")
 def delete_consumable(db: Session = Depends(get_db),
                       lang: str = Depends(get_lang),
                       consumable: ConsumableModel = Depends(check_consumable_exists)):
--- backend/core/translation.py
+++ backend/core/translation.py
@@ -7,16 +7,18 @@ MESSAGES = {
         "consumable.not_found": "Расходник с таким id не найден",
         "consumable.get_all": "Список расходников успешно передан",
         "common.validation_error": "Ошибка валидаци введенных данных",
-        "common.internal_error": "Внутренняя ошибка сервера"
+        "common.internal_error": "Внутренняя ошибка сервера",
+        "common.no_upd_data": "Нет данных для обновления"
     },
     "en":{
         "consumable.created": "Consumable successfully added",
         "consumable.updated": "Consumable successfully updated",
         "consumable.deleted": "Consumable successfully deleted",
-        "consumable.get": "Consumable data successfully returned",
+        "consumable.get": "Consumable data returned successfully",
         "consumable.not_found": "Consumable with this id not found",
-        "consumable.get_all": "Consumables list successfully returned",
+        "consumable.get_all": "Consumables list returned successfully",
         "common.validation_error": "Validation error",
-        "common.internal_error": "Internal serever error"
+        "common.internal_error": "Internal serever error",
+        "common.no_upd_data": "No data to update"
     }
 }
